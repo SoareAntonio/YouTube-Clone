8677c9318aa75ca25f7c2d73af31c4bf630f74b4
Soare Antonio <antonio.soare.42005@gmail.com>
2025-05-13 23:45:14 +0300

    video add

--- scripts/index.js
+++ scripts/index.js
@@ -10,10 +10,153 @@ const channels=[{
     views: "4.2M",
     uploaded: "6 months ago",
     subscriberCount: "15M"
-  }];
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-2.avif",
+    videoTime: "8:51",
+    videoLink: "https://www.youtube.com/watch?v=FwHu3122pbc",
+    profilePicture: "images/channel-pictures/channel-2.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-2.jpg",
+    channelLink: "https://www.youtube.com/@VlogDeIT",
+    title: "Tutorial mini-aplicatie de vreme (1 din 3) + giveaway",
+    author: "Vlog de IT",
+    views: "2.4K",
+    uploaded: "6 years ago",
+    subscriberCount: "22.3K"
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-3.avif",
+    videoTime: "1:39",
+    videoLink: "https://www.youtube.com/watch?v=wzSTWbv7XlY",
+    profilePicture: "images/channel-pictures/channel-3.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-3.jpg",
+    channelLink: "https://www.youtube.com/@Mr.PiwPiew",
+    title: `The Difference Between "C" Languages in 90 seconds`,
+    author: "Mr.PiwPiew",
+    views: "336K",
+    uploaded: "9 months ago",
+    subscriberCount: "43.3K",
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-4.avif",
+    videoTime: "0:58",
+    videoLink: "https://www.youtube.com/watch?v=rR4n-0KYeKQ",
+    profilePicture: "images/channel-pictures/channel-4.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-4.jpg",
+    channelLink: "https://www.youtube.com/@jomakaze",
+    title: "how we write/review code in big tech companies",
+    author: "Joma Tech",
+    views: "15M",
+    uploaded: "5 years ago",
+    subscriberCount: "2.3M"
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-5.avif",
+    videoTime: "6:45",
+    videoLink: "https://www.youtube.com/watch?v=UHyX9zBluMw&t=1s",
+    profilePicture: "images/channel-pictures/channel-5.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-5.jpg",
+    channelLink: "https://www.youtube.com/@armankhondker",
+    title: "Day in the Life of a Software Engineer at TikTok (Seattle)",
+    author: "Arman Khondker",
+    views: "1.6M",
+    uploaded: "2 years ago",
+    subscriberCount: "29.3K"
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-6.avif",
+    videoTime: "7:04",
+    videoLink: "https://www.youtube.com/watch?v=97i2BAUw5Xc&t=1s",
+    profilePicture: "images/channel-pictures/channel-6.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-6.jpg",
+    channelLink: "https://www.youtube.com/@LowLevelTV",
+    title: "before you code, learn how computers work",
+    author: "Low Level",
+    views: "769K",
+    uploaded: "1 years ago",
+    subscriberCount: "843K"
+  },{
+    thumbnailSrc: "images/thumbnails/thumbnail-7.avif",
+    videoTime: "6:31:23",
+    videoLink: "https://www.youtube.com/watch?v=G3e-cpL7ofc&t=22066s",
+    profilePicture: "images/channel-pictures/channel-8.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-8.jpg",
+    channelLink: "https://www.youtube.com/@SuperSimpleDev",
+    title: "HTML & CSS Full Course - Beginner to Pro",
+    author: "SuperSimpleDev",
+    views: "14M",
+    uploaded: "3 years ago",
+    subscriberCount: "643K"
+  },{
+    thumbnailSrc: "images/thumbnails/thumbnail-8.avif",
+    videoTime: "10:35",
+    videoLink: "https://www.youtube.com/watch?v=XQeKBLRPpxA&t=11s",
+    profilePicture: "images/channel-pictures/channel-7.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-7.jpg",
+    channelLink: "https://www.youtube.com/@ColeCaccamise",
+    title: "I tricked my brain to be ADDICTED to coding.",
+    author: "Cole Caccamise",
+    views: "129K",
+    uploaded: "2 months ago",
+    subscriberCount: "47.1K"
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-9.avif",
+    videoTime: "23:08",
+    videoLink: "https://www.youtube.com/watch?v=BJatgOiiht4&t=4s",
+    profilePicture: "images/channel-pictures/channel-9.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-9.jpg",
+    channelLink: "https://www.youtube.com/@fknight",
+    title: "7 Design Patterns EVERY Developer Should Know",
+    author: "ForrestKnight",
+    views: "590K",
+    uploaded: "5 months ago",
+    subscriberCount: "645K"
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-10.avif",
+    videoTime: "12:17",
+    videoLink: "https://www.youtube.com/watch?v=E8lXC2mR6-k",
+    profilePicture: "images/channel-pictures/channel-10.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-10.jpg",
+    channelLink: "https://www.youtube.com/@Nova_Designs_",
+    title: "Master React JS in easy way",
+    author: "Nova Designs",
+    views: "33.2K",
+    uploaded: "9 months ago",
+    subscriberCount: "212K"
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-11.avif",
+    videoTime: "5:44",
+    videoLink: "https://www.youtube.com/watch?v=J0NuOlA2xDc&t=59s",
+    profilePicture: "images/channel-pictures/channel-11.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-11.jpg",
+    channelLink: "https://www.youtube.com/@coderized",
+    title: "Never install locally",
+    author: "Codorized",
+    views: "2M",
+    uploaded: "2 years ago",
+    subscriberCount: "186K"
+  },
+{
+    thumbnailSrc: "images/thumbnails/thumbnail-12.avif",
+    videoTime: "21:58",
+    videoLink: "https://www.youtube.com/watch?v=cuHDQhDhvPE&t=562s",
+    profilePicture: "images/channel-pictures/channel-12.jpg",
+    profilePictureTooltip: "/images/channel-pictures/channel-12.jpg",
+    channelLink: "https://www.youtube.com/@Fireship",
+    title: "I built the same app 10 times // Which JS Framework is best?",
+    author: "Fireship",
+    views: "2.7M",
+    uploaded: "3 years ago",
+    subscriberCount: "3.9M"
+  },
+];
 
+let channelHTML='';
   channels.forEach((channel) => {
-  const html = `
+    channelHTML+= `
     <div class="video-preview">
       <div class="thumbnail-row">
         <a href="${channel.videoLink}" target="_blank" class="video-title-link">
@@ -54,6 +197,5 @@ const channels=[{
       </div>
     </div>
   `;
-
-  document.querySelector('.videos-container').innerHTML += html;
 });
+document.querySelector('.js-channel-grid').innerHTML=channelHTML;
